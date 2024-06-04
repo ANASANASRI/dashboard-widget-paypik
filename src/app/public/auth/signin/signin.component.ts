9018715b9ebfe3f19644aaf5ce4ddcd09b52e849
anas <anasanasri3030@gmail.com>
2024-06-04 09:10:42 +0100

    sign in

--- src/app/public/auth/signin/signin.component.ts
+++ src/app/public/auth/signin/signin.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DatetimeHelper } from 'src/app/_core/helpers/datetime.helper';
 import { CommonService } from 'src/app/_core/services/common.service';
 import { AdminRoutes } from 'src/app/admin/admin.routes';
-import { AppRoutes } from 'src/app/app.routes';
 import { pageTransition } from 'src/app/shared/utils/animations';
 import { Images } from 'src/assets/data/images';
 import { AlertType } from '../../../shared/components/alert/alert.type';
 import { PublicRoutes } from '../../public.routes';
+import { TokenService } from '../token.service';
+import { AuthService } from '../auth.service';
+import { Signin } from './signin.model';
 
 @Component({
   selector: 'app-signin',
@@ -18,7 +20,7 @@ import { PublicRoutes } from '../../public.routes';
 })
 export class SigninComponent {
   readonly signinBannerImage: string = Images.bannerLogo;
-
+  marchandId!: number;
   isLoading: boolean = false;
   readonly publicRoutes = PublicRoutes;
   readonly currentYear: number = DatetimeHelper.currentYear;
@@ -33,30 +35,101 @@ export class SigninComponent {
   constructor(
     public commonService: CommonService,
     private formBuilder: FormBuilder,
-    private router: Router
+    private router: Router,
+    private authService: AuthService,
+    private tokenService: TokenService,
+    private route: ActivatedRoute,
   ) {}
+
+  ngOnInit(): void {
+    // Récupérer les paramètres de la route
+    this.route.params.subscribe(params => {
+      this.marchandId = +params['id'];
+    });
+  }
+
   protected readonly AlertType = AlertType;
 
-  protected onFormSubmitHandler = (event: SubmitEvent) => {
-    event.preventDefault();
-    this.isLoading = true;
-
-    setTimeout(() => {
-      this.isLoading = false;
-      this.router.navigate([AppRoutes.Admin, AdminRoutes.Dashboard]);
-    }, 3000);
-  };
-
-  protected onAlertCloseHandler = (e: any) => {
+  onAlertCloseHandler(event: any): void {
+    // Réinitialiser les erreurs du serveur lors de la fermeture de l'alerte
     this.serverErrors = [];
-  };
+  }
+
+  onSubmit(): void {
+    console.log(this.signInForm.value);
+  
+    const username = this.signInForm.value.username;
+    const password = this.signInForm.value.password;
+  
+    if (username && password) {
+      const signinData: Signin = { username, password };
+  
+      this.authService.signin(signinData).subscribe(
+        (data) => {
+          console.log('Access Token:', data.accessToken);
+          console.log('User Roles:', data.roles);
+          this.tokenService.saveToken(data.accessToken);
+          
+
+          if(data.roles.includes('ROLE_ADMIN')) {
+            this.redirectBasedOnRole(data.roles);
+
+          } else if (data.roles.includes('ROLE_MARCHAND')) {
+            // Si l'utilisateur est marchand, appeler findMarchandIdByMarchandName avant de rediriger
+            this.authService.findMarchandIdByMarchandName(username).subscribe(
+              (marchandId) => {
+                this.marchandId = marchandId;
+                console.log('Marchand ID:', marchandId);
+                this.router.navigate(['/marchand/dashboard/' + this.marchandId]);
+              },
+              (error) => {
+                console.error('Error fetching marchand ID:', error);
+                // Gérer les erreurs ici...
+                alert('An error occurred while fetching the marchand ID. Please try again.');
+              }
+            );
+          } else {
+            // Pour les autres rôles, rediriger en fonction du rôle
+            this.redirectBasedOnRole(data.roles);
+          }
+        },
+        (err) => {
+          console.log('Login Error:', err);
+          if (err.status === 401) {
+            alert('Email or password is incorrect.');
+          } else {
+            alert('An error occurred during login. Please try again.');
+          }
+        }
+      );
+    } else {
+      alert('Please enter both username and password.');
+      console.log('Username or password is not defined');
+    }
+  }
+
+
+  
+  // Fonction pour rediriger l'utilisateur en fonction de son rôle
+  private redirectBasedOnRole(roles: string[]): void {
+    if (roles.includes('ROLE_ADMIN')) {
+      this.router.navigate(['/admin/dashboard']);
+      
+    
+    } else if (roles.includes('ROLE_COMMERCIAL') ) {
+      this.router.navigate(['/commercial/validation']);
+      
+    } else {
+      this.router.navigate(['/signup']);
+    }
+  }
 
   /// Forget password
-
   modalOpen: boolean = false;
-
 
   togglePassForget() {
       this.modalOpen = !this.modalOpen;
   }
+
+
 }
